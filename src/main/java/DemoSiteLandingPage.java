a8507a492aa255d1c20b7ce58c1a4e96198d71ba
Your Name <jacob.baordman.jb@gmail.com>
2019-01-30 09:23:08 +0000

    created landing page to get all the elements i need

---
+++ src/main/java/DemoSiteLandingPage.java
@@ -0,0 +1,19 @@
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class DemoSiteLandingPage {
+	
+	@FindBy(name = "username")
+	private WebElement user;
+	
+	@FindBy(name = "password")
+	private WebElement pass;
+	
+	@FindBy(name = "FormsButton2")
+	private WebElement button;
+	
+	public void createAndLogin(String user, String pass) {
+		this.user.sendKeys(user);
+		this.pass.sendKeys(pass);
+	}
+}
